6773d94f1d8fc3bc300fe9356e87b84adfd0eb02
gabrielaboy <gabeaboy@gmail.com>
2018-02-16 20:33:44 -0500

    AWS lambda ses boilerplate

--- handler.js
+++ handler.js
@@ -1,20 +1,85 @@
 'use strict';
-const AWS = require('aws-sdk'),
-    dynamoDbClient = new AWS.DynamoDB.DocumentClient(),
-    uuid = require('uuid');
-// var proxy = require('proxy-agent');
-// AWS.config.update({
-//     httpOptions: {
-//         agent: proxy('https://687969368484.signin.aws.amazon.com/console')
-//     }
-// });
-//AWS.configure.update({ region: 'us-west-2' })
-console.log('my-service.handler')
-module.exports.createMovies = (event, context, callback) => {
-    let TableName;
-    const params = {
-        TableName: 'Task'
-    }
-    console.log(dynamoDbClient.scan(params))
-    return dynamoDbClient.scan(params).promise();
-}
+
+module.exports.sendReminderDaily = (event, context, callback) => {
+    
+    var AWS = require('aws-sdk');
+    AWS.config.update({region:'us-east-1'});
+    var ses = new AWS.SES();
+    var fs = require('fs');
+
+    var emailHtml = fs.readFileSync('./dailyReminder.html', 'utf-8');
+
+    var toAndFromAdress = 'verifiedemail@yourdomain.com'
+    var params = {
+        Destination: {
+            ToAddresses: [toAndFromAdress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8", 
+                    Data: emailHtml
+                }, 
+                Text: {
+                    Charset: "UTF-8", 
+                    Data: "Remember to continue helping the Woof Garden in your Pluralsight course!"
+                }
+            }, 
+            Subject: {
+                Charset: "UTF-8", 
+                Data: "Woof Garden Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAdress],
+        Source: toAndFromAdress, 
+    };
+
+    ses.sendEmail(params, function(err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack); 
+        // successful response
+        else callback(null, data);
+    }); 
+};
+
+module.exports.sendReminderWeekend = (event, context, callback) => {
+    
+    var AWS = require('aws-sdk');
+    AWS.config.update({region:'us-east-1'});
+    var ses = new AWS.SES();
+    var fs = require('fs');
+
+    var emailHtml = fs.readFileSync('./weekendReminder.html', 'utf-8');
+
+    var toAndFromAdress = 'verifiedemail@yourdomain.com'
+    var params = {
+        Destination: {
+            ToAddresses: [toAndFromAdress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8", 
+                    Data: emailHtml
+                }, 
+                Text: {
+                    Charset: "UTF-8", 
+                    Data: "Here's a weekend Remember that puppies are adorable!!"
+                }
+            }, 
+            Subject: {
+                Charset: "UTF-8", 
+                Data: "Woof Garden Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAdress],
+        Source: toAndFromAdress, 
+    };
+
+    ses.sendEmail(params, function(err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack); 
+        // successful response
+        else callback(null, data);
+    }); 
+};
